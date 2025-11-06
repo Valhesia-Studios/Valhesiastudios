55391601833871ac339027c9cc5e5b5d7dda28f3
Valhesia-Studios <studiosvalhesia@gmail.com>
2025-11-06 09:28:05 -0300

    Site valhesia Studio

---
+++ script.js
@@ -0,0 +1,9 @@
+// Faz aparecer suavemente ao rolar
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+});
+document.querySelectorAll('.fade-in').forEach(el => observer.observe(el));
